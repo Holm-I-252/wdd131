019f60306c55e1fe5ac72a5ccd6660267ea9f1d5
Ian Holm <ianholm252@gmail.com>
2025-04-23 11:15:08 -0600

    2/23/25

---
+++ running2.js
@@ -0,0 +1,7 @@
+const myApples = 4;
+const friendsApples = 2;
+let total = myApples + friendsApples;
+
+document.getElementById("myAppleElement").innerHTML = myApples;
+document.getElementById("friendAppleElement").innerHTML = friendsApples;
+document.getElementById("totalApplesElement").innerHTML = total;
