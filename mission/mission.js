9f7451ef1e8c509df264d221ef99938e50589eb5
Ian Holm <ianholm252@gmail.com>
2025-05-05 19:06:04 -0600

    Update 5/5/25

--- mission/mission.js
+++ mission/mission.js
@@ -1,10 +1,13 @@
 let light_dark = document.getElementById("light_dark")
+let pic = document.getElementById("logo")
 
 function lightDark() {
     if (light_dark.value == "dark") {
         document.body.classList.add("dark")
+        pic.setAttribute("src", "byui-logo_white.png")
     } else {
         document.body.classList.remove("dark")
+        pic.setAttribute("src", "byui-logo_blue.webp")
     }
 }
 
